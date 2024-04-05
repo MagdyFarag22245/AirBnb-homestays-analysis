efe25eb0dca5202e8c451312bf89030d58a3cc4a
MagdyFarag22245 <118448337+MagdyFarag22245@users.noreply.github.com>
2024-04-06 01:12:41 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,4 +10,4 @@ st.markdown(''' <center>  <h6>
     This app is created to analyze the data of New York Airbnb company </center> </h6> ''', unsafe_allow_html=True)
 
 # the path of a photo provided from the path of this file
-st.image(r"D:\Study\Epsilon DS\Bnb_project\Sources\d3181322e4522cf897fa8c1a038c6a2d.gif")
+st.image(r"https://i.pinimg.com/originals/d3/18/13/d3181322e4522cf897fa8c1a038c6a2d.gif")
